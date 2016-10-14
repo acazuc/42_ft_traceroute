530f381fe38dbeb49e40a598621c2df27fd6cb4c
Jukino <acazuc@student.42.fr>
2016-10-14 20:53:59 +0200

    Changed to 5 sec timeout

--- src/route.c
+++ src/route.c
@@ -99,7 +99,7 @@ void route(t_env *env)
 {
 	struct timeval tv;
 
-	tv.tv_sec = 1;
+	tv.tv_sec = 5;
 	tv.tv_usec = 0;
 	if (setsockopt(env->socket, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0)
 	{
